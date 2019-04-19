25ca3b95cc7493db93c757d957201a6f42de2c51
zhoujun <zhoujun@knowbox.cn>
2019-04-19 11:48:06 +0800

    file add

--- CoreAnimation/CoreAnimation/ViewController.m
+++ CoreAnimation/CoreAnimation/ViewController.m
@@ -11,23 +11,55 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong)  UIView *view1;
+@property (nonatomic, strong)  UIView *view2;
+@property (nonatomic, strong)  UIView *view3;
+@property (nonatomic, strong)  UIView *view4;
+
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.view.layer addAnimation:[self backgroundColorAnimation] forKey:@"backgroundColorAnimation"];
+    self.view1 = [[UIView alloc] init];
+    self.view1.frame = CGRectMake(0, 100, CGRectGetWidth(self.view.bounds) * 0.5, 100);
+    self.view2 = [[UIView alloc] init];
+    self.view2.frame = CGRectMake(CGRectGetWidth(self.view.bounds) * 0.5, 100, CGRectGetWidth(self.view.bounds) * 0.5, 100);
+    self.view3 = [[UIView alloc] init];
+    self.view3.frame = CGRectMake(0, 200, CGRectGetWidth(self.view.bounds) * 0.5, 100);
+    self.view4 = [[UIView alloc] init];
+    self.view4.frame = CGRectMake(CGRectGetWidth(self.view.bounds) * 0.5, 200, CGRectGetWidth(self.view.bounds) * 0.5, 100);
     
-    CALayer *a = [[CALayer alloc] init];
-    a.frame = CGRectMake(100, 100, 100, 100);
-    a.backgroundColor = [UIColor blueColor].CGColor;
+    self.view1.backgroundColor = UIColor.lightGrayColor;
+    self.view2.backgroundColor = UIColor.lightTextColor;
+    self.view3.backgroundColor = UIColor.lightTextColor;
+    self.view4.backgroundColor = UIColor.lightGrayColor;
     
-    [self.view.layer addSublayer:a];
+    [self.view addSubview:self.view1];
+    [self.view addSubview:self.view2];
+    [self.view addSubview:self.view3];
+    [self.view addSubview:self.view4];
     
+    UIImage *image = [UIImage imageNamed:@"test_carnival_unlocked"];
+    [self addImage:image withContentRect:CGRectMake(0, 0, 0.5, 0.5) ￼toLayer:self.view1.layer];
+    [self addImage:image withContentRect:CGRectMake(0.5, 0, 0.5, 0.5) ￼toLayer:self.view2.layer];
+    [self addImage:image withContentRect:CGRectMake(0, 0.5, 0.5, 0.5) ￼toLayer:self.view3.layer];
+    [self addImage:image withContentRect:CGRectMake(0.5, 0.5, 0.5, 0.5) ￼toLayer:self.view4.layer];
+    
+}
 
+- (void)addImage:(UIImage *)image withContentRect:(CGRect)rect ￼toLayer:(CALayer *)layer {
+    layer.contents = (__bridge id)image.CGImage;
     
+    //scale contents to fit
+    layer.contentsGravity = kCAGravityResizeAspect;
+    
+    //set contentsRect 在图层边框里显示寄宿图的一个子域
+    layer.contentsRect = rect;
 }
 
 
